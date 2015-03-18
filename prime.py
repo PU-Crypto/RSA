3268aa75b86f07c57b6be761e6698f6b4c222178
Melvin Höfges <melvin.hoefges@hotmail.de>
2015-03-18 22:10:02 +0100

    PRIME

---
+++ prime.py
@@ -0,0 +1,23 @@
+#NEW PRIME GENERATOR
+#CULLEN PRIMES
+#
+
+def fak(a): #fakultät
+	b = a
+	while b > 0:
+		a = a*b
+		b -= b 
+	return a
+
+
+cap = int(input("Grenze : "))
+
+x = 0
+
+prime = []
+for x in range(cap+1):
+	prime.append(x*(2**x)+1)
+	prime.append(fak(fak(x**2))+1)
+
+print(prime)
+
