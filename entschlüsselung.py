c416c42ca47867865ce893540ee1d2b57d9f4e4a
Melvin Höfges <melvin.hoefges@hotmail.de>
2015-04-11 17:38:47 +0200

    UTF-8 Support hinzugefügt

--- entschlüsselung.py
+++ entschlüsselung.py
@@ -1,17 +1,34 @@
 #Entschlüsselung
 
-c = int(input("Crypted Text:"))
 
 with open("privatekey.txt")as rfile:
 	lines = rfile.readlines()[0:2]
-
 n = int(lines[0])
-
 d = int(lines[1])
 
+with open("cypher.txt")as rfile:
+	grenze = int(rfile.readlines()[0])
 
-c=c**d
-m=(c%n)
+with open("cypher.txt")as rfile:
+		sammlung = rfile.readlines()[1:int(grenze)+1]
 
-print("Original Number:")
-print(m)
+for x in range(0,len(sammlung)):
+	sammlung[x] = int(sammlung[x])
+
+for x in range(0,len(sammlung)):
+	sammlung[x]=sammlung[x]**d
+	sammlung[x]=sammlung[x]%n
+
+print("Original Array:")
+print(sammlung)
+
+text = ''
+
+for x in range(0,len(sammlung)):
+	sammlung[x] = chr(sammlung[x])
+	text = text+sammlung[x]
+
+print(text)
+
+with open("Truetext.txt", "w")as rfile:
+	print(text, file =rfile)
--- verschlüsseln.py
+++ verschlüsseln.py
@@ -2,7 +2,16 @@
 
 import sys
 
-m = int(input("TrueText:"))
+text = input("TrueText als Buchstaben:")
+
+text=text.encode('utf-8')
+list(text)
+sammlung = []
+
+for i in range(0,len(text)): #erstelle einen array mit zahlen 
+        dump=text[i]
+        sammlung.append(dump)
+print(sammlung)
 
 with open("publicKey.txt")as rfile:
 	lines = rfile.readlines()[0:2]
@@ -10,8 +19,16 @@ with open("publicKey.txt")as rfile:
 n = int(lines[0])
 e = int(lines[1])
 
-m=m**e
-c=(m%n)
 
-print("Crypted Number:")
-print(c)
+for x in range(0,(len(sammlung))):
+	sammlung[x]=sammlung[x]**e
+	sammlung[x]=(sammlung[x]%n)
+
+print("Crypted list:")
+
+print(sammlung)
+
+with open("cypher.txt", "w") as text_file :
+	print(str(len(sammlung)), file=text_file)
+	for x in range(0,len(sammlung)):
+		print(str(sammlung[x]), file=text_file)
