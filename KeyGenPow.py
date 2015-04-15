81109106483ffcd052bb6edc737720dc3fc57489
Melvin Höfges <melvin.hoefges@hotmail.de>
2015-04-15 20:41:21 +0200

    FINAL BUILD
    SEHR große Primzahlen sehr schnell,
    Schlüsselberechnung mit Test verschlüsselung ausgestattet,
    Zuverlässig abgesehen von umlauten.

--- KeyGenPow.py
+++ KeyGenPow.py
@@ -4,6 +4,15 @@
 
 import random
 
+def extendedGcd(a,b):
+		u, v, s, t = 1, 0, 0, 1
+		while b>0:
+			r=a//b 
+			a, b = b, a-r*b 
+			u, s = s, u-r*s
+			v, t = t, v-r*t
+		return   u
+
 def _try_composite(a, d, n, s):
     if pow(a, d, n) == 1:
         return False
@@ -38,6 +47,37 @@ def is_prime(n, _precision_for_huge_n=16):
     # otherwise
     return not any(_try_composite(a, d, n, s) 
                    for a in _known_primes[:_precision_for_huge_n])
+#für test verschlüsslung
+def int2baseTwo(x):
+    """x is a positive integer. Convert it to base two as a list of integers
+    in reverse order as a list."""
+    # repeating x >>= 1 and x & 1 will do the trick
+    assert x >= 0
+    bitInverse = []
+    while x != 0:
+        bitInverse.append(x & 1)
+        x >>= 1
+    return bitInverse
+ 
+ 
+def modExp(a, d, n):
+    """returns a ** d (mod n)"""
+    assert d >= 0
+    assert n >= 0
+    base2D = int2baseTwo(d)
+    base2DLength = len(base2D)
+    modArray = []
+    result = 1
+    for i in range(1, base2DLength + 1):
+        if i == 1:
+            modArray.append(a % n)
+        else:
+            modArray.append((modArray[i - 2] ** 2) % n)
+    for i in range(0, base2DLength):
+        if base2D[i] == 1:
+            result *= base2D[i] * modArray[i]
+    return result%n
+#ende für test verschlüsselung
 
 begin = int(input('Anfang : '))
 grenze = begin + int(input('Grenze, begin + x : '))
@@ -49,43 +89,53 @@ _known_primes = [2, 3]
 
 _known_primes += [x for x in range(begin, grenze, 2) if is_prime(x)]
 
-print(_known_primes) #for testing purposes enabled
+#print(_known_primes) #for testing purposes enabled
 #Key GEN :
 n = [1]
 n2 = [1]
 
 print ('Primzahlen generiert schlüssel berechnung läuft...')
 		
-d = (-1)
+i = True
 
-while d < 0 :
 
-	p = random.choice(_known_primes)
-	q = random.choice(_known_primes)
+while i:
+	d = (-1)
 
-	if p == q:
+	while d < 0 :
+
+		p = random.choice(_known_primes)
 		q = random.choice(_known_primes)
+		print('Primzahlen ausgewählt')
 
-	n[0] = (p*q)
+		if p == q:
+			q = random.choice(_known_primes)
 
-	n2[0] = (p-1)*(q-1)
+		n[0] = (p*q)
 
-	e = (2**(2*random.randint(2,50))+1) #random Fermet nummer also 2^(2*n)+1
-	while e > n2[0] or e < (n2[0]*0.25):
-		e = (2**(2*random.randint(2,50))+1) #random Fermet nummer also 2^(2*n)+1
-	#e =  random.randint(1, n2[0]) 
+		print('N errechnet')
 
-	#euklidischer algorithmus, quelle : GOOGLE ^^
-	def extendedGcd(a,b):
-		u, v, s, t = 1, 0, 0, 1
-		while b>0:
-			r=a//b 
-			a, b = b, a-r*b 
-			u, s = s, u-r*s
-			v, t = t, v-r*t
-		return   u
+		n2[0] = (p-1)*(q-1)
+		print('N2 errechnet')
 
-	d=extendedGcd(e, n2[0])
+		e =  random.randint(1, n2[0]) 
+		print('E gewählt')		
+
+		d=extendedGcd(e, n2[0])
+		print('Inverse "D" errechnet')
+
+	print('Test verschlüsselung zur funktions gewährleistung wird durchgeführt...')
+
+	numberTrueOne = 666
+
+	numberCypher = [1]
+
+	numberCypher[0] = modExp(numberTrueOne, e, n[0])
+	numberTrueTwo = modExp(numberCypher[0], d, n[0])
+
+	if numberTrueOne == numberTrueTwo :
+		print('Erfolgreich!')
+		i = False
 
 print("N: "+ str(n[0]))
 print("E: " + str(e))
